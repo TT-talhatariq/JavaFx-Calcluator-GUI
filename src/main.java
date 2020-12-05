1ea70316ff86349b04af07c704a030ee2ce98e82
Talha Tariq <67586874+TT-talhatariq@users.noreply.github.com>
2020-12-05 11:04:46 +0500

    Create main.java

---
+++ src/main.java
@@ -0,0 +1,42 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Calculator");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public static class Model {
+        public float calculate(long first_nbr, long second_nbr, String operator){
+            switch (operator) {
+                case "+":
+                    return first_nbr + second_nbr;
+                case "-":
+                    return first_nbr - second_nbr;
+                case "*":
+                    return first_nbr * second_nbr;
+                case "/":
+                    if (second_nbr == 0)
+                        return 0;
+                    return first_nbr / second_nbr;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
